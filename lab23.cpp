ee9588e66d98755e48b32404640c1d50b615bfdd
romsaejao <76035596+romsaejao@users.noreply.github.com>
2022-02-08 14:27:37 +0700

    Update lab23.cpp

--- lab23.cpp
+++ lab23.cpp
@@ -1,4 +1,4 @@
-// [Missing Code 1] Include header file.
+#include"lab23.h"
   
 int main(){
 	srand(time(0));
@@ -9,18 +9,29 @@ int main(){
 	Unit hero("Hero",name);
 	
 	Equipment sword(0,8,4);
-	// [Missing Code 2]  Create Equipment axes, shield and armor here
-
+	Equipment axes(0,16,-3);
+	Equipment shield(0,-1,7);
+	Equipment armor(25,-2,2);
 	
-	
-	char eq;	
+    char eq;	
 	cout << " [1] Sword \n [2] Axes \n [3] Shield \n [4] Armor \n";
 	cout << "Please selet your equipment: ";
 	cin >> eq;
-	// [Missing Code 3] Equip a selected equipment to the hero. 
-
-	
-	
+	switch(eq) 
+		       {
+        		case '1' :
+        			hero.equip(&sword);
+        			break;
+        		case '2' :
+        			hero.equip(&axes);
+        			break;
+        		case '3' :
+        			hero.equip(&shield);
+        			break;
+        		case '4' :
+        			hero.equip(&armor);
+        			break;
+		        }
 	Unit mons("Monster","Kraken");
 	
 	int turn_count = 1;
@@ -57,7 +68,21 @@ int main(){
 			cout << " [1] Sword \n [2] Axes \n [3] Shield \n [4] Armor \n";
 			cout << "Please select your equipment: ";
 			cin >> eq;
-			// [Missing Code 3] Equip a selected equipment to the hero.
+			switch(eq) 
+		        {
+        			case '1' :
+        				hero.equip(&sword);
+        				break;
+        			case '2' :
+        				hero.equip(&axes);
+        				break;
+        			case '3' :
+        				hero.equip(&shield);
+        				break;
+        			case '4' :
+        				hero.equip(&armor);
+        				break;
+		        }
 
 			
 		}
